75a42e8809a1342b143d1ae05fa7cf26ac9a5ab4
mikasa <1244456115@qq.com>
2026-04-11 16:51:58 +0800

    fix: attempt to fix inject exception

--- src/main/java/cn/sh1rocu/goety/mixin/common/PersistentEntitySectionManagerMixin.java
+++ src/main/java/cn/sh1rocu/goety/mixin/common/PersistentEntitySectionManagerMixin.java
@@ -15,7 +15,7 @@ public class PersistentEntitySectionManagerMixin<T extends EntityAccess> {
     // method_31863: addWorldGenChunkEntities.1
     // method_31864: addLegacyChunkEntities.0
     @Inject(method = {"method_31857", "method_31863", "method_31864"}, at = @At("TAIL"))
-    private static void goety$addedToWorld(EntityAccess entityAccess, CallbackInfo ci) {
+    private void goety$addedToWorld(EntityAccess entityAccess, CallbackInfo ci) {
         if (entityAccess instanceof IEntityListener entity)
             entity.onAddedToWorld();
     }
